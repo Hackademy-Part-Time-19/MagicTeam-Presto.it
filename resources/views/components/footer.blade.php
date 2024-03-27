7b8e94130a45b55e9a148729a3c3b165bc7c2be8
LivioaraBerbecaru <148262791+LivioaraBerbecaru@users.noreply.github.com>
2024-03-27 19:19:30 +0100

    prima del pull

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,26 +1,27 @@
 <footer>
-    <div class="footer">
-        <div class="cont-footer">
-            <div class="bottom">
-                <div class="cent">
-                <div class="iccons">
-                  <a  href=""><img style="width: 5vh;" src="images/facebook.png" alt=""></a>
-                  <a href=""><img style="width: 4vh;" src="images/instagram.png" alt=""></a>
-                  <a href=""><img style="width: 4vh;" src="images/twitter.png" alt=""></a>
-                  <a href=""><img src="images/youtube.png" alt=""></a>
-                </div>
-                <div class="container-list">
-                    <a href="">Home</a>
-                    <a href="">Contact us</a>
-                    <a href="">Our Team</a>
-                    <a href="">Lavora con noi</a>
+  <div class="footer">
+    <div class="cont-footer">
+      <div class="bottom">
+        <div class="cent">
+          <div class="iccons">
+            <a  href=""><img style="width: 5vh;" src="images/facebook.png" alt=""></a>
+            <a href=""><img style="width: 4vh;" src="images/instagram.png" alt=""></a>
+            <a href=""><img style="width: 4vh;" src="images/twitter.png" alt=""></a>
+            <a href=""><img src="images/youtube.png" alt=""></a>
+          </div>
+          <div class="container-list">
+            <a href="">Home</a>
+            <a href="">Contact us</a>
+            <a href="">Our Team</a>
+            <a href="">Lavora con noi</a>
 
-              </div>
-              <div class="copy">
-                <h5>Copyright ©2024; Designed by <span>MagicTeam</span></h5>
-              </div>
-            </div>
+          </div>
+          <div class="copy">
+            <h5>Copyright ©2024; Designed by <span>MagicTeam</span></h5>
+          </div>
         </div>
-       
+      </div>
     </div>
+       
+  </div>
 </footer>    
