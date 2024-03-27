92697eaa19a52190bb04c57d248ccdf1ca3b61a4
LivioaraBerbecaru <148262791+LivioaraBerbecaru@users.noreply.github.com>
2024-03-27 19:01:19 +0100

    footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,26 @@
+<footer>
+    <div class="footer">
+        <div class="cont-footer">
+            <div class="bottom">
+                <div class="cent">
+                <div class="iccons">
+                  <a  href=""><img style="width: 5vh;" src="images/facebook.png" alt=""></a>
+                  <a href=""><img style="width: 4vh;" src="images/instagram.png" alt=""></a>
+                  <a href=""><img style="width: 4vh;" src="images/twitter.png" alt=""></a>
+                  <a href=""><img src="images/youtube.png" alt=""></a>
+                </div>
+                <div class="container-list">
+                    <a href="">Home</a>
+                    <a href="">Contact us</a>
+                    <a href="">Our Team</a>
+                    <a href="">Lavora con noi</a>
+
+              </div>
+              <div class="copy">
+                <h5>Copyright ©2024; Designed by <span>MagicTeam</span></h5>
+              </div>
+            </div>
+        </div>
+       
+    </div>
+</footer>    
