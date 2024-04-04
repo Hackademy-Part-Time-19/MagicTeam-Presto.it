f037c8fa4d8229da606eb040aff6c39a922285a8
Framandala <aq506997@gmail.com>
2024-04-04 20:42:56 +0200

    resizing terminato

---
+++ app/Jobs/resizeImage.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Jobs;
+
+use Spatie\Image\Image;
+use Spatie\Image\Enums\Fit;
+use Illuminate\Bus\Queueable;
+use Spatie\Image\Manipulations;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class resizeImage implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private $fileName;
+    private $path;
+
+    public function __construct($filePath , private $w , private $h)
+    {
+        $this->path = dirname($filePath);
+        $this->fileName = basename($filePath);
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $w = $this->w;
+        $h = $this->h;
+        $srcPath = storage_path() . '/app/public/' . $this->path . '/' . $this->fileName;
+        $destPath = storage_path() . '/app/public/' . $this->path . "/crop_{$w}x{$h}_" . $this->fileName;
+        
+
+        $croppedImage = Image::load($srcPath)
+                        ->crop(Manipulations::CROP_CENTER , $w , $h,)
+                        ->save($destPath);
+     }
+}
