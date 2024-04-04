a1a9ebd942baccdcb584b950b2669a100900447f
Federica Reina <federicareina@MBP-di-Federica.homenet.telecomitalia.it>
2024-04-04 18:33:33 +0200

    f441833

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,4 +1,4 @@
-<footer>
+{{-- <footer>
 
     <div class="footer">
 
@@ -28,8 +28,8 @@
                         
                     </div>
                    <div>
-                   {{--  <x-flag-country-us style="width: 30px; height:30px"/>
-                    <x-flag-country-fr style="width: 30px; height:30px"/> --}}
+                    <x-flag-country-us style="width: 30px; height:30px"/>
+                    <x-flag-country-fr style="width: 30px; height:30px"/> 
                    </div>
                     <div class="copy">
                         <h5>Copyright ©2024; Designed by <span>MagicTeam</span></h5>
@@ -39,5 +39,36 @@
 
         </div>
 
+    </div>
+</footer>
+ --}}
+
+
+<footer>
+    <div style="background-color:rgb(28, 28, 28)">
+        <div class="col-lg-8 mx-auto col-10 text-center">
+            <div class="row">
+                <div class="col">
+                    <img src="/img/presto_white.svg" alt="" style="width: 120px;margin-bottom:20px;margin-top:20px;">
+                </div>
+                <div class="col">
+                    <a class="nav-link aFooter" href="/">Home</a>
+                </div> 
+                
+                <div class="col">
+                <a class="nav-link aFooter" href="{{ route('article.index') }}">Annunci</a>
+            </div>
+                @guest
+                <div class="col">
+                    <a href=" {{ route('login') }}"class="aFooter">Lavora con noi</a>
+                </div>
+                @endguest
+                @auth
+                <div class="col">
+                    <a href=" {{ route('revisor.request') }}"class="nav-link aFooter ">Lavora con noi</a>
+                    </div>
+                @endauth
+            </div>
+        </div>
     </div>
 </footer>
